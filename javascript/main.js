ad812ad913600ebdf3026bdef430bdd2c733abd8
Giampaolo1 <giampaolo.r@outlook.com>
2019-10-16 14:38:20 +0200

    img

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,6 @@
 // Artista e brano
 
-document.getElementById('nowplay').innerHTML='ARTISTA E BRANO';
+// document.getElementById('scriptartista').innerHTML='ARTISTA';
+// document.getElementById('scriptbrano').innerHTML='<span>Brano</span>';
+
+document.getElementById('scriptartistabrano').innerHTML='<h6>Artista</h6> <h6>Brano</h6>';
